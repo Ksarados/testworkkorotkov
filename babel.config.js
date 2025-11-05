eaf1c5db77b479ede65ec690029e4c36d5817930
Andrey Korotkow <andreyrule@gmail.com>
2025-11-05 13:27:44 +0300

    added the FSD structure and alias

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,29 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        extensions: [
+          '.ios.js',
+          '.android.js',
+          '.js',
+          '.jsx',
+          '.ts',
+          '.tsx',
+          '.json',
+        ],
+        alias: {
+          '@app': './src/app',
+          '@processes': './src/processes',
+          '@pages': './src/pages',
+          '@widgets': './src/widgets',
+          '@features': './src/features',
+          '@entities': './src/entities',
+          '@shared': './src/shared',
+        },
+      },
+    ],
+  ],
 };
