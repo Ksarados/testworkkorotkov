fd2a6d6b9560cf87571fe5114793cb9732c4a0de
Andrey Korotkow <andreyrule@gmail.com>
2025-11-06 15:17:56 +0300

    fix arrow right and safe area view

--- src/shared/assets/icons/ArrowRightIcon.tsx
+++ src/shared/assets/icons/ArrowRightIcon.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import Svg, { SvgProps, Path } from 'react-native-svg';
-const ArrowRightIcon = (props: SvgProps) => (
+import Svg, { SvgProps, G, Path, Defs, ClipPath } from 'react-native-svg';
+const SvgComponent = (props: SvgProps) => (
   <Svg
     xmlns='http://www.w3.org/2000/svg'
-    width={5}
-    height={10}
+    width={6}
+    height={12}
     fill='none'
     {...props}
   >
-    <Path stroke='#fff' strokeWidth={1.5} d='m.53.53 4 4-4 4' />
+    <G clipPath='url(#a)'>
+      <Path stroke='#fff' strokeWidth={1.5} d='m1 3 4 4-4 4' />
+    </G>
+    <Defs>
+      <ClipPath id='a'>
+        <Path fill='#fff' d='M0 0h6v14H0z' />
+      </ClipPath>
+    </Defs>
   </Svg>
 );
-export default ArrowRightIcon;
+export default SvgComponent;
